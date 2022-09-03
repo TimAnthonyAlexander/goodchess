2b558f01fbd0f09770568f92788c784d79c8249b
TimAnthonyAlexander <github@timanthonyalexander.de>
2022-09-03 21:08:39 +0200

    Chess: Update databasefiller
    This update took 18 minutes .

--- databasefiller.php
+++ databasefiller.php
@@ -14,9 +14,38 @@ for ($i = 0; $i < 100; $i++) {
     $board->initializeDefaultBoard();
     $color = true;
     for ($j = 0; $j < 50; $j++) {
-        $bestMove = TimFish::bestMove($board, $color, 2, 15);
+        $randomBool = random_int(0, 3) !== 1;
+        if ($randomBool){
+            $bestMove = TimFish::bestMove($board, $color, 2, 4);
+            print "Playing best move.".PHP_EOL;
+        } else {
+            $boardArray = $board->jsonSerialize();
+            $pieces = [];
+            $allMovesForPieces = [];
+            foreach ($boardArray as $letter => $col) {
+                foreach ($col as $number => $piece) {
+                    if ($piece === null) {
+                        continue;
+                    }
+                    if ($piece['color'] === $color) {
+                        $allMovesForPieces[$piece['uuid']] = $allMovesForPiece = (new Rules())->getAllMovesForPiece($board, new Position($letter, $number));
+                        if (count($allMovesForPiece) > 0) {
+                            $pieces[] = [$letter, $number, $piece['uuid']];
+                        }
+                    }
+                }
+            }
+
+            $randomPiece = $pieces[random_int(0, count($pieces)-1)];
+            $allMoves = $allMovesForPieces[$randomPiece[2]];
+
+            $bestMove = $allMoves[random_int(0, count($allMoves)-1)];
+
+            print "Playing random move.".PHP_EOL;
+        }
         print "Moving [".($j+1)."]: " . $bestMove . PHP_EOL;
         $board->movePiece($bestMove);
+        $board->viewTerminal();
         $color = !$color;
         sleep(4);
     }
